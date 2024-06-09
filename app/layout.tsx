74fbd0fa678bd5ce7070c7aab4a2ead1baeb06cd
Purav Saluja <123455357+Purav001@users.noreply.github.com>
2024-06-09 21:04:38 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Urify",
+  description: "Generated for Batch System as an assignment",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="max-w-10xl mx-auto">
+          <div>
+          <Navbar/>
+          </div>
+          {children}
+          <Footer/>
+        </main>
+      </body>
+    </html>
+  );
+}
